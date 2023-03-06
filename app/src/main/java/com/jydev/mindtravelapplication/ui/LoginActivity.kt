f6e30c71e82817b7a3a9bf265a85e91939871f93
jaeyoung <kjy0302014@gmail.com>
2023-03-06 10:04:36 +0900

    Initial commit

---
+++ app/src/main/java/com/jydev/mindtravelapplication/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.jydev.mindtravelapplication.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jydev.mindtravelapplication.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
