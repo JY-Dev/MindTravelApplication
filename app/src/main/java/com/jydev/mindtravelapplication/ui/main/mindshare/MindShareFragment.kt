6ce5403c6215a4d8c5d28e6828f3b26bdf9e8733
jaeyoung <kjy0302014@gmail.com>
2023-03-20 17:39:53 +0900

    mind share item 레이아웃 작업

---
+++ app/src/main/java/com/jydev/mindtravelapplication/ui/main/mindshare/MindShareFragment.kt
@@ -0,0 +1,10 @@
+package com.jydev.mindtravelapplication.ui.main.mindshare
+
+import com.jydev.mindtravelapplication.base.BaseFragment
+import com.jydev.mindtravelapplication.databinding.FragmentMindShareBinding
+
+class MindShareFragment : BaseFragment<FragmentMindShareBinding>(FragmentMindShareBinding::inflate) {
+    override fun onViewCreateLifeCycle() {
+
+    }
+}
