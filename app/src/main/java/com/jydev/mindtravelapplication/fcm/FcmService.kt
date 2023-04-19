895ec6f5298b5bce5f5dbf3ca5dc500773199c18
jaeyoung <kjy0302014@gmail.com>
2023-04-19 09:48:11 +0900

    FCM notification pending intent 설정

--- app/src/main/java/com/jydev/mindtravelapplication/fcm/FcmService.kt
+++ app/src/main/java/com/jydev/mindtravelapplication/fcm/FcmService.kt
@@ -2,7 +2,9 @@ package com.jydev.mindtravelapplication.fcm
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -23,12 +25,14 @@ class FcmService : FirebaseMessagingService() {
     }
 
     private fun sendNotification(messageBody: String) {
-
+        val intent = Intent()
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
         val channelId = "fcm_channel"
         val notificationBuilder = NotificationCompat.Builder(this, channelId)
             .setSmallIcon(R.drawable.clover)
             .setContentTitle("테스트")
             .setContentText(messageBody)
+            .setContentIntent(pendingIntent)
             .setAutoCancel(true)
 
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
